88679587432fdce719aeb902c05341a24b99a898
GulsenGenc <gulsengenctuncer@gmail.com>
2022-04-23 09:26:55 +0300

    23.04.2022

---
+++ src/depoYönetimi/Main.java
@@ -0,0 +1,10 @@
+package depoYönetimi;
+
+import static depoYönetimi.Islemler.girisMenusu;
+
+public class Main {
+    public static void main(String[] args) {
+
+         girisMenusu();
+    }
+}
---
+++ src/depoYönetimi/UrunBilgileri.java
@@ -0,0 +1,31 @@
+package depoYönetimi;
+
+public class UrunBilgileri {
+
+    public String urunIsmi;
+    public String uretici;
+    public String birimi;
+    public int ıd;
+    public double miktar;
+    public String rafNo;
+
+    public UrunBilgileri() {
+    }
+
+    public UrunBilgileri(String urunIsmi, String uretici, String birimi, int ıd) {
+        this.urunIsmi = urunIsmi;
+        this.uretici = uretici;
+        this.birimi = birimi;
+        this.ıd = ıd;
+    }
+
+    @Override
+    public String toString() {
+        return  "urunIsmi='" + urunIsmi + '\'' +
+                ", uretici='" + uretici + '\'' +
+                ", birimi='" + birimi + '\'' +
+                ", ıd=" + ıd +
+                ", miktar=" + miktar +
+                ", rafNo=" + rafNo;
+    }
+}
